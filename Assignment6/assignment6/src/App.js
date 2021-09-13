b3f5e71344e3a162a339d437d540e311c039df75
Mihir <mihirvora1053@gmail.com>
2021-09-13 19:48:46 +0530

    second

--- Assignment6/assignment6/src/App.js
+++ Assignment6/assignment6/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Pdff from "./image/MihirVoraResume.pdf";
+import ReactPDF from "../node_modules/@intelllex/react-pdf";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactPDF       url={Pdff}
+      showProgressBar={false}
+      showToolbox={false} />
+
     </div>
   );
 }
